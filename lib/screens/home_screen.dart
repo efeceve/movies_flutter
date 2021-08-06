4f0adc9c0923e151dfac630a5d8c97d30ec183f5
Felipe <felipecartagenavasquez@gmail.com>
2021-08-06 16:34:36 -0400

    Added home and details screen. Modified home screen

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:peliculas/widgets/widgets.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Movies at display'),
+          elevation: 0,
+          actions: [
+            IconButton(onPressed: () {}, icon: Icon(Icons.search_outlined))
+          ],
+        ),
+        body: Column(
+          children: [
+            CardSwiper()
+            // Movies Horizontal List
+          ],
+        ));
+  }
+}
